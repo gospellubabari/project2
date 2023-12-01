c35ffca082ce1b2c0272fd5009da9565b0e9206c
Gospel <gospellubabari@gmail.com>
2023-12-01 21:43:06 +0700

    Refactor: changed alert to red background colour for error input

--- scripts.js/script.js
+++ scripts.js/script.js
@@ -9,39 +9,56 @@ messageForm.addEventListener('submit', (event) => {
   const emailInputTag = document.getElementById('email');
   const messageTextAreaTag = document.getElementById('message');
 
-//   if (fullName == '') {
-//     alert('please provide your full name.');
-//   }else if (email == ''){
-//     alert('please provide your email.');
-//   }else if (message == ''){
-//     alert('please provide your message.');
-//   }else{
-//     messageForm.submit();
-//   }
 
-let fullName = fullNameInputTag.value;
-let email = emailInputTag.value;
-let messsage = messageTextAreaTag.value;
+  //   if (fullName == '') {
+  //     alert('please provide your full name.');
+  //   }else if (email == ''){
+  //     alert('please provide your email.');
+  //   }else if (message == ''){
+  //     alert('please provide your message.');
+  //   }else{
+  //     messageForm.submit();
+  //   }
+
+  fullNameInputTag.style.backgroundColor = '#fff';
+  emailInputTag.style.backgroundColor = '#fff';
+  messageTextAreaTag.style.backgroundColor = '#fff';
+
+
+  let fullName = fullNameInputTag.value;
+  let email = emailInputTag.value;
+  let messsage = messageTextAreaTag.value;
 
 
 
-if (fullName && email && message){
-  //send the following values to the server to save
- // messageForm.submit()
- //fullName
- //email
- //message
+  if (fullName && email && message) {
+    //send the following values to the server to save
+    // messageForm.submit()
+    //fullName
+    //email
+    //message
 
- //when the server responds true, alert the user of success
- successMessageBox.innerHTML = `Hello ${fullName}, your response has been recorded.`;
- successMessageBox.style.display = 'block';
+    //when the server responds true, alert the user of success
+    successMessageBox.innerHTML = `Hello ${fullName}, your response has been recorded.`;
+    successMessageBox.style.display = 'block';
 
- //reset fields
-  fullNameInputTag.value = '';
- emailInputTag.value = '';
- messageTextAreaTag.value = '';
+    //reset fields
+    fullNameInputTag.value = '';
+    emailInputTag.value = '';
+    messageTextAreaTag.value = '';
 
-}else{
-  alert('All fields are required. Please provide them')
-}
+  } else {
+
+    if (fullName == '') {
+      fullNameInputTag.style.backgroundColor = 'rgb(255 0 0 / 15%)';
+    } else if (email == '') {
+      emailInputTag.style.backgroundColor = 'rgb(255 0 0 / 15%)';
+    } else if (message == '') {
+      messageTextAreaTag.style.backgroundColor = 'rgb(255 0 0 / 15%)';
+    } else {
+      pass;
+    }
+
+  }
+
 })
